09ed712bb98a0c60b5e2f5c480bfc092cfa49bc0
Neelam Thakur <neelam@Neelams-Air.lan>
2024-12-28 00:39:43 -0600

    CB-December2024: JWT and refresh token

--- core/routers.py
+++ core/routers.py
@@ -1,11 +1,20 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets.login import LoginViewSet
+
+
 
 
 router = routers.SimpleRouter()
 
 
 router.register(r'user', UserViewSet, basename='user')
+
+########################## AUTH #######################
+
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls,
