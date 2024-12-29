e77f5d0cd31ddb12019ac8d3439ded8a50953b1e
Neelam Thakur <neelam@Neelams-Air.lan>
2024-12-28 19:50:05 -0600

    refresh token changes

--- core/routers.py
+++ core/routers.py
@@ -2,6 +2,7 @@ from rest_framework import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
+from core.auth.viewsets.refresh import RefreshViewSet
 
 
 
@@ -15,6 +16,7 @@ router.register(r'user', UserViewSet, basename='user')
 
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,
